de7567f35706a46e5d006086d618a23399b70bcd
Leovaldez42 <gaurav4.037@gmail.com>
2020-05-08 19:00:04 +0530

    basic editor made

---
+++ src/editor/editor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+// import debounce from '../helpers';
+// import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class EditorComponent extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render () {
+        const { classes } = this.props;
+
+        return(
+        <div className={classes.editorContainer}>
+            <ReactQuill></ReactQuill>
+        </div>
+        )
+    }
+}
+
+export default withStyles(styles)(EditorComponent)
--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore')
+
+  firebase.initializeApp({    
+    apiKey: "AIzaSyC8qfOEIYxmuWRfkIlLHDvapNXkZcoOAxM",
+    authDomain: "evernote-8f119.firebaseapp.com",
+    databaseURL: "https://evernote-8f119.firebaseio.com",
+    projectId: "evernote-8f119",
+    storageBucket: "evernote-8f119.appspot.com",
+    messagingSenderId: "264373563560",
+    appId: "1:264373563560:web:ab2f8bf5fd1bcc5118d29d",
+    measurementId: "G-GP164ZPS9P"
+  });
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
